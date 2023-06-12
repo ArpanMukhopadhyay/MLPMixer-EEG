1bca594db1ede8a6d15c4a9aad0a841690f4393d
ArpanMukhopadhyay <amukhopadhyay02@gmail.com>
2023-06-12 13:05:49 +0930

    Remove multiple files

--- iterator.m
+++
@@ -1,7 +0,0 @@
-path1 = '/Users/arpanmukhopadhyay/Documents/EEG topics/sleep-edfx-toolbox-master/markers/1';
-path2 = '/Users/arpanmukhopadhyay/Documents/EEG topics/sleep-edfx-toolbox-master/markers/2';
-path3 = '/Users/arpanmukhopadhyay/Documents/EEG topics/sleep-edfx-toolbox-master/markers/3';
-path4 = '/Users/arpanmukhopadhyay/Documents/EEG topics/sleep-edfx-toolbox-master/markers/4';
-path5 = '/Users/arpanmukhopadhyay/Documents/EEG topics/sleep-edfx-toolbox-master/markers/5';
-
-count = 0;
